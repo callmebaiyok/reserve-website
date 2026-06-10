2a83b886666b5049dedd2d5b07732358a78d48f9
callmebaiyok <callmebaiyok090@gmail.com>
2026-06-10 10:07:32 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 export default defineConfig({
+  base: '/reserve-website/',
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // ✅ เพิ่มบรรทัดนี้
-    },
-  },
 })
